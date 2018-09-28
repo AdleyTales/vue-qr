0f7a625b95ad4336fb318d35a28dd41f31f96aef
adleytales <adleytales@126.com>
2018-09-28 22:18:25 +0800

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="adley-index">
+        <section class="top">
+            <input type="file" id="file_1">
+            <br>
+            <mt-button type="primary"> 开始上传 </mt-button>
+            <span>请上传文件</span>
+        </section>
+        <section>
+
+        </section>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+    #adley-index {
+        width: 800px;
+        height: 200px;
+        border: 2px dashed #26a2ff;
+        /* background: #eeddee; */
+        margin: 0 auto;
+        padding: 30px;
+        /* box-shadow: 0px 0px 1px #26a2ff; */
+    }
+
+    #adley-index #file_1 {
+        margin-bottom: 30px;
+    }
+
+    #adley-index .top {
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
+
+    #adley-index .top span {
+        position: absolute;
+        font-size: 80px;
+        color: aliceblue;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50% , -50%);
+        
+    }
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+Vue.use(Mint)
+
+import { Button } from 'mint-ui';
+Vue.component(Button.name, Button);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
